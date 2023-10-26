7866376caf7f7adf4424a7a101e458c6dae5f6b4
SophiaTretiakova <122561236+SophiaTretiakova@users.noreply.github.com>
2023-10-27 01:19:37 +0300

    hw done

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,14 +3,13 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign({}, objA, objB);
 }
 
 // const obj1 = { name: 'John' };
 // const obj2 = { age: 30 };
 // const mergedObj = merge(obj1, obj2);
-
 // console.log(mergedObj); // { name: 'John', age: 30 }
 
 export {};
