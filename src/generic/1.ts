3e9afe59038996a84df8b568b3b9747612d4fa51
SophiaTretiakova <122561236+SophiaTretiakova@users.noreply.github.com>
2023-10-26 19:46:45 +0300

    generic

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,14 +3,15 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
+type MyType = string | number;
+
+function getPromise<T>(): Promise<T[]> {
   return new Promise((resolve) => {
-    resolve(['Text', 50]);
+    resolve(["Text", 50] as T[]);
   });
 }
 
-getPromise()
-.then((data) => {
+getPromise<MyType>().then((data) => {
   console.log(data);
 });
 
--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,8 +3,14 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
-  return Object.assign(objA, objB);
+function merge<T, U>(objA: T, objB: U): T & U {
+  return Object.assign({}, objA, objB);
 }
 
-export {}
+// const obj1 = { name: 'John' };
+// const obj2 = { age: 30 };
+// const mergedObj = merge(obj1, obj2);
+
+// console.log(mergedObj); // { name: 'John', age: 30 }
+
+export {};
